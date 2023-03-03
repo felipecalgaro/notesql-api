1fc4a1b88ed4cb86ab56e492106f6a26f763fa63
Felipe <pipecalgaro@gmail.com>
2023-03-03 18:44:10 -0300

    feat: add tests on note resolver

---
+++ src/graphql/note/resolver.spec.ts
@@ -0,0 +1,91 @@
+import { describe, expect, it } from "vitest";
+import { InMemoryNotesRepository } from "../../../test/in-memory-repositories/in-memory-notes-repository";
+import { getNoteResolver } from "./resolver";
+import { makeNote } from "../../../test/factory/note-factory";
+
+describe("note resolver", () => {
+  const inMemoryNotesRepository = new InMemoryNotesRepository();
+  const noteResolver = getNoteResolver(inMemoryNotesRepository);
+
+  it("should be able to get notes", async () => {
+    await inMemoryNotesRepository.writeNote(makeNote());
+    await inMemoryNotesRepository.writeNote(makeNote());
+
+    const users = await noteResolver.Query.getNotes();
+
+    expect(users.length).toBe(2);
+    expect(users).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ title: "My note" }),
+        expect.objectContaining({ title: "My note" }),
+      ])
+    );
+  });
+
+  it("should be able to write a note", async () => {
+    const note = makeNote("test-password", 8);
+
+    await noteResolver.Mutation.writeNote(undefined, note);
+
+    const notes = await inMemoryNotesRepository.getNotes();
+
+    expect(notes).toHaveLength(3);
+    expect(notes).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          id: 8,
+          author: expect.objectContaining({ password: "test-password" }),
+        }),
+      ])
+    );
+  });
+
+  it("should be able to prioritize a note", async () => {
+    await inMemoryNotesRepository.writeNote(makeNote("123", 10));
+    await inMemoryNotesRepository.writeNote(makeNote("123", 11));
+
+    await noteResolver.Mutation.prioritizeNote(undefined, {
+      id: 10,
+      priority: true,
+    });
+
+    const notes = await inMemoryNotesRepository.getNotes();
+
+    expect(notes).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: 10, priority: true }),
+        expect.objectContaining({ id: 11, priority: false }),
+      ])
+    );
+  });
+
+  it("should be able to update a note status", async () => {
+    await inMemoryNotesRepository.writeNote(makeNote("1234", 12));
+    await inMemoryNotesRepository.writeNote(makeNote("123", 13));
+
+    await noteResolver.Mutation.updateStatus(undefined, {
+      id: 13,
+      status: "FINISHED",
+    });
+
+    const notes = await inMemoryNotesRepository.getNotes();
+
+    expect(notes).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: 12, status: "UNFINISHED" }),
+        expect.objectContaining({ id: 13, status: "FINISHED" }),
+      ])
+    );
+  });
+
+  it("should be able to delete a note", async () => {
+    await noteResolver.Mutation.deleteNote(undefined, {
+      id: 12,
+    });
+
+    const notes = await inMemoryNotesRepository.getNotes();
+
+    expect(notes).toHaveLength(6);
+    expect(notes.find((note) => note.id === 12)).toBeUndefined();
+  });
+});
--- src/graphql/note/resolver.ts
+++ src/graphql/note/resolver.ts
@@ -1,10 +1,6 @@
 import { DateTimeResolver } from "graphql-scalars";
-import { Note } from "../../entities/note";
+import { Note, Status } from "../../entities/note";
 import { INotesRepository } from "../../repositories/notes-repository";
-
-interface MutationArgs {
-  id: string;
-}
 
 export function getNoteResolver(repository: INotesRepository) {
   return {
@@ -14,12 +10,12 @@ export function getNoteResolver(repository: INotesRepository) {
     },
     Mutation: {
       writeNote: async (_: any, args: Note) => await repository.writeNote(args),
-      prioritizeNote: async (_: any, { id }: MutationArgs) =>
-        await repository.prioritizeNote(id),
-      updateStatus: async (_: any, { id }: MutationArgs) =>
-        await repository.updateStatus(id),
-      deleteNote: async (_: any, { id }: MutationArgs) =>
-        await repository.deleteNote(id),
+      prioritizeNote: async (_: any, args: { id: number; priority: boolean }) =>
+        await repository.prioritizeNote(args.id, args.priority),
+      updateStatus: async (_: any, args: { id: number; status: Status }) =>
+        await repository.updateStatus(args.id, args.status),
+      deleteNote: async (_: any, args: { id: number }) =>
+        await repository.deleteNote(args.id),
     },
   };
 }
