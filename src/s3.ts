0f0854df4a1444a34855cbb766539687cceb76a0
Felipe <pipecalgaro@gmail.com>
2023-06-25 11:22:16 -0300

    feat: install aws-sdk and configure upload to s3

---
+++ src/s3.ts
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+import fs from "fs";
+import S3 from "aws-sdk/clients/s3";
+
+dotenv.config();
+
+const bucketName = process.env.AWS_BUCKET_NAME;
+const region = process.env.AWS_BUCKET_REGION;
+const accessKeyId = process.env.AWS_ACCESS_KEY;
+const secretAccessKey = process.env.AWS_SECRET_KEY;
+
+const s3 = new S3({
+  region,
+  accessKeyId,
+  secretAccessKey,
+});
+
+interface FileProps {
+  path: string;
+  filename: string;
+}
+
+export function uploadFile(file: FileProps) {
+  const fileStream = fs.createReadStream(file.path);
+
+  const uploadParams = {
+    Bucket: bucketName!,
+    Body: fileStream,
+    Key: file.filename,
+  };
+
+  return s3.upload(uploadParams).promise();
+}
